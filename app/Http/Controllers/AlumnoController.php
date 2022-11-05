95bfc98b34540d8b7fc7859eb3e4e16621337c3a
AMFernan <fernandoam1122@gmai.com>
2022-11-05 12:30:03 -0700

    Editar y Eliminar Alumnos

--- app/Http/Controllers/AlumnoController.php
+++ app/Http/Controllers/AlumnoController.php
@@ -1,15 +1,55 @@
 <?php
 
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use App\Models\Alumno;
-
-class AlumnoController extends Controller
+    namespace App\Http\Controllers;
+    
+    use App\Models\Alumno;
+    use Illuminate\Http\Request;
+    
+    class AlumnoController extends Controller
 {
     function consultar(){
         $alumnos = Alumno::paginate(3);
 
         return view('Alumnos.alumnos', compact('alumnos'));
     }
+
+    function registrar(){
+        return view('Alumnos.registrar');
+    }
+
+    function guardar(Request $datos){
+        $alumno = Alumno::create($datos->post());
+
+        return redirect('/alumnos');
+    }
+
+    function eliminar ($id){
+        $alumno= Alumno::find($id);
+        $alumno->delete();
+
+        return redirect('/alumnos');
+    }
+
+    function editar ($id){
+        $alumno= Alumno::find($id);
+        
+        return view('Alumnos.editar', compact('alumno'));
+    }
+
+    function actualizar(Request $datos, $id) {
+        $alumno= Alumno::find($id);
+        $alumno->n_control = $datos->input('n_control');
+        $alumno->nombre = $datos->input('nombre');
+        $alumno->edad = $datos->input('edad');
+        $alumno->sexo = $datos->input('sexo');
+        $alumno->sexo = $datos->input('sexo');
+        $alumno->fecha_nacimiento = $datos->input('fecha_nacimiento');
+        $alumno->domicilio = $datos->input('domicilio');
+        $alumno->telefono = $datos->input('telefono');
+        $alumno->save();
+
+        return redirect('/alumnos');
+    }
+
 }
+
--- resources/views/Alumnos/alumnos.blade.php
+++ resources/views/Alumnos/alumnos.blade.php
@@ -2,6 +2,10 @@
 
 @section('contenido')
     <h1>Listado de alumnos</h1><hr>
+
+    <a href="{{ url('/alumno/registrar') }}" class="btn btn-secondary">Registrar Alumno</a><br><br>
+
+
     <table class="table table-hover">
         <thead>
             <th>ID</th>
@@ -25,8 +29,8 @@
                 <td>{{$a->domicilio}}</td>
                 <td>{{$a->telefono}}</td>
                 <td>
-                <a href="">editar</a>   
-                <a href="">eliminar</a>
+                <a href="{{url('/alumno/editar')}}/{{$a->id}}" class="btn btn-primary btn-sm">Editar</a>   
+                <a href="{{url('/alumno/eliminar')}}/{{$a->id}}" class="btn btn-danger btn-sm">Eliminar</a>
                 </td>
             </tr>               
             @endforeach
@@ -34,4 +38,5 @@
     <div class="text-center">
         {{$alumnos->links()}}
     </div>
+
 @stop
